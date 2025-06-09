98f12dfbcc539eb49f22a3448fca10447bd7a877
a.semenov <a.semenov@trueengineering.ru>
2025-06-09 17:17:30 +0700

    Test 2

---
+++ selenium_fast_tests/test1.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+url = "https://portal.trueengineering.ru/"
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get(url=url)  # открываем страницу
+time.sleep(2)
+driver.back() # возвращаемся назад
+time.sleep(2)
+driver.forward() # навигация вперед
+time.sleep(1)
+driver.refresh() # обновление страницы
+time.sleep(1)
+
+driver.close() # Closes the current window
+driver.quit() # Closes the browser and shuts down the ChromiumDriver executable
