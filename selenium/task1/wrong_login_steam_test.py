70d5a6e67041ebd01df30b5ee0bdd5339a60a3a8
SenenovAntonAQA <antsenenov@gmail.com>
2025-06-17 23:11:54 +0700

    small_fix (last)

--- selenium/task1/wrong_login_steam_test.py
+++ selenium/task1/wrong_login_steam_test.py
@@ -10,7 +10,8 @@ LOGIN_HREF = (By.XPATH, "//a[contains(@class, 'global_action_link')]")
 USERNAME = (By.XPATH, "//input[@type='text']")  # поле ввода имени аккаунта
 PASS = (By.XPATH, "//input[@type='password']")  # поле ввода пароля
 SUBMIT_BUT = (By.XPATH, "//button[@type='submit']")  # кнопка войти
-WRONG_CREDENTIALS = (By.XPATH, "//button[@type='submit']/../following-sibling::div[string-length(normalize-space(text())) > 1]")
+WRONG_CREDENTIALS = (By.XPATH,
+                     "//button[@type='submit']/../following-sibling::div[string-length(normalize-space(text())) > 1]")
 DEFAULT_TIMEOUT = 10
 
 
@@ -31,7 +32,8 @@ def test_try_login_to_steam(driver):
     # Click login button.
 
     fake_username = Faker().user_name()
-    fake_password = Faker().password(length=10, special_chars=True, digits=True, upper_case=True)
+    fake_password = Faker().password(length=10, special_chars=True, digits=True,
+                                     upper_case=True)
 
     wait.until(EC.element_to_be_clickable(USERNAME)).send_keys(fake_username)
     wait.until(EC.element_to_be_clickable(PASS)).send_keys(fake_password)
@@ -40,7 +42,8 @@ def test_try_login_to_steam(driver):
     wait.until(EC.element_to_be_clickable(SUBMIT_BUT)).click()
     # Click sign in button.
 
-    actual_text = wait.until(EC.visibility_of_element_located(WRONG_CREDENTIALS)).text
+    actual_text = wait.until(
+        EC.visibility_of_element_located(WRONG_CREDENTIALS)).text
     expected_text = "Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова."
 
     assert actual_text == expected_text, f"После ввода некорректных (рандомных) данных ожидали текст {expected_text}, а получили {actual_text}"
