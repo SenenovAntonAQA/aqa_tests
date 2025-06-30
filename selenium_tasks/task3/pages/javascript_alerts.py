29094175e6797831156dc0e083d313b1127a5a10
SenenovAntonAQA <antsenenov@gmail.com>
2025-06-30 22:20:31 +0700

    fast fix 2nd test case

--- selenium_tasks/task3/pages/javascript_alerts.py
+++ selenium_tasks/task3/pages/javascript_alerts.py
@@ -1,78 +1,52 @@
 from selenium_tasks.task3.pages.base_page import BasePage
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from enum import StrEnum
 
 
 class JSAlertsPage(BasePage):
-    HEROKUAPP_URL = "https://the-internet.herokuapp.com/"
-    DEFAULT_TIMEOUT = 10
-    CONGRATULATION_XPATH = (By.XPATH, "//*[@id='content']//p")
     BUTTON_JS_ALERT = (By.XPATH, "//*[@onclick='jsAlert()']")
     BUTTON_JS_CONFIRM = (By.XPATH, "//*[@onclick='jsConfirm()']")
     BUTTON_JS_PROMPT = (By.XPATH, "//*[@onclick='jsPrompt()']")
     RESULT_JAVASCRIPT_ALERTS = (
         By.XPATH, "//*[@id='result' and normalize-space() != '']")
 
-    def alert_appearance(self):
-        """Ожидание появления алёрта."""
-        self.wait.until(EC.alert_is_present())
+    def text_in_result(self):
+        """Отображение текста из поля 'Result'."""
+        return self.wait.until(
+            EC.visibility_of_element_located(self.RESULT_JAVASCRIPT_ALERTS)
+        ).text
 
     def click_js_alert(self):
         """Кликаем на кнопку JS Alert."""
         self.wait.until(
             EC.element_to_be_clickable(self.BUTTON_JS_ALERT)
         ).click()
-        self.alert_appearance()
+        self.wait.until(EC.alert_is_present())
+
+    def text_in_alert(self):
+        """Выводим текст алёрта."""
+        alert = self.wait.until(lambda d: d.switch_to.alert)
+        return alert.text
+
+    def click_js_confirm(self):
+        """Кликаем на кнопку JS Confirm."""
+        self.wait.until(
+            EC.element_to_be_clickable(self.BUTTON_JS_CONFIRM)
+        ).click()
+        self.wait.until(EC.alert_is_present())
+
+    def click_js_prompt(self):
+        """Кликаем на кнопку JS Prompt."""
+        self.wait.until(
+            EC.element_to_be_clickable(self.BUTTON_JS_PROMPT)
+        ).click()
+        self.wait.until(EC.alert_is_present())
+
+    def send_text_to_alert(self, text):
+        alert = self.wait.until(lambda d: d.switch_to.alert)
+        alert.send_keys(text)
 
     def alert_accept(self):
         """Берем в фокус алёрт и нажимаем на "Принять" / "Ок"."""
         alert = self.wait.until(lambda d: d.switch_to.alert)
         alert.accept()
-        # В Alert нажать кнопку “OK“
-
-        check_text_in_result(wait, expected_result_alert_text)
-        # Alert закрылся. В секции “Result“
-        # отображается текст “You subccessfuly
-        # clicked an alert
-
-        wait.until(EC.element_to_be_clickable(BUTTON_JS_CONFIRM)).click()
-        # Нажать на кнопку “Click for JS Confirm“
-
-        alert_appearance(wait)
-        alert = wait.until(lambda d: d.switch_to.alert)
-        alert_text_verification(alert, expected_confirm_text)
-        # Отображается Alert с текстом “I am a
-        # JS Confirm“
-
-        alert.accept()
-        # В Alert нажать кнопку “OK“
-
-        check_text_in_result(wait, expected_result_confirm_text)
-        # Alert закрылся. В секции “Result“
-        # отображается текст “You clicked: Ok“
-
-        wait.until(EC.element_to_be_clickable(BUTTON_JS_PROMPT)).click()
-        # Нажать кнопку “Click for JS Prompt“
-
-        alert_appearance(wait)
-        alert = wait.until(lambda d: d.switch_to.alert)
-        alert_text_verification(alert, expected_prompt_text)
-        # Отображается Alert с текстом “I am a
-        # JS Prompt“
-
-        random_text = Faker().sentence(nb_words=2)
-
-        alert.send_keys(random_text)
-        alert.accept()
-        # Ввести в поле ввода случайно
-        # сгенерированный текст. Нажать кнопку
-        # “OK“
-
-        check_text_in_result(wait, f"You entered: {random_text}")
-        # Alert закрылся. В секции “Result“
-        # отображается текст “You entered:
-        # %введенный случайно
-        # сгенерированный текст%“
--- selenium_tasks/task3/tests/test_alerts.py
+++ selenium_tasks/task3/tests/test_alerts.py
@@ -1,94 +1,93 @@
 from selenium_tasks.task3.pages.javascript_alerts import JSAlertsPage
 from selenium_tasks.task3.utils.config_reader import ConfigReader
-
-def alert_appearance(wait):
-    assert wait.until(EC.alert_is_present()), "Алёрт не появился"
+from faker import Faker
 
 
-def alert_text_verification(alert, text):
-    assert alert.text == text, (f"Текст алерта ({alert.text}) не соответствует"
-                                f" {text}")
-
-
-def check_text_in_result(wait, expected_text):
-
-    element = wait.until(
-        EC.visibility_of_element_located(RESULT_JAVASCRIPT_ALERTS)
+def alert_text_verification(alert_text, expect_text):
+    assert alert_text == expect_text, (
+        f"Текст алерта ({alert_text}) не соответствует {expect_text}"
     )
-    actual_text = element.text
+
+
+def check_text_in_result(actual_text, expected_text):
     assert actual_text == expected_text, (
         f"В секции 'Result' отображается некорректный текст: '{actual_text}'. "
         f"Ожидалось: '{expected_text}'"
     )
 
+
 def test_2_alerts(browser):
-
     alert_page = JSAlertsPage(browser)
-
-    expected_alert_text = "I am a JS Alert"
-    expected_confirm_text = "I am a JS Confirm"
-    expected_prompt_text = "I am a JS prompt"
-    # специально некорректный текст? Prompt -> prompt
-
-    expected_result_alert_text = "You successfully clicked an alert"
-    # специально некорректный текст? subccessfuly -> successfully
-    expected_result_confirm_text = "You clicked: Ok"
-
 
     browser.get(ConfigReader.get("urls.alerts"))
 
-    JSAlertsPage.click_js_alert()
+    alert_page.click_js_alert()
     # Нажать на кнопку “Click for JS Alert“
 
-    alert_appearance(wait)
-    alert = wait.until(lambda d: d.switch_to.alert)
-    alert_text_verification(alert, expected_alert_text)
-    # Отображается Alert с текстом “I am a
-    # JS Alert“
+    expected_alert_text = "I am a JS Alert"
 
-    alert.accept()
+    alert_text_verification(
+        alert_text=alert_page.text_in_alert(),
+        expect_text=expected_alert_text
+    )
+    # Проверяем отображение текста алёрта "I am a JS Alert“
+
+    alert_page.alert_accept()
     # В Alert нажать кнопку “OK“
 
-    check_text_in_result(wait, expected_result_alert_text)
-    # Alert закрылся. В секции “Result“
-    # отображается текст “You subccessfuly
+    expected_result_alert_text = "You successfully clicked an alert"
+    check_text_in_result(
+        actual_text=alert_page.text_in_result(),
+        expected_text=expected_result_alert_text
+    )
+    # Alert закрылся. В секции “Result“ отображается текст “You subccessfuly
     # clicked an alert
 
-    wait.until(EC.element_to_be_clickable(BUTTON_JS_CONFIRM)).click()
+    # специально некорректный текст в ОР ТК: subccessfuly -> successfully ?)))
+
+    alert_page.click_js_confirm()
     # Нажать на кнопку “Click for JS Confirm“
 
-    alert_appearance(wait)
-    alert = wait.until(lambda d: d.switch_to.alert)
-    alert_text_verification(alert, expected_confirm_text)
-    # Отображается Alert с текстом “I am a
-    # JS Confirm“
+    expected_confirm_text = "I am a JS Confirm"
+    alert_text_verification(
+        alert_text=alert_page.text_in_alert(),
+        expect_text=expected_confirm_text
+    )
+    # Отображается Alert с текстом “I am a JS Confirm“
 
-    alert.accept()
+    alert_page.alert_accept()
     # В Alert нажать кнопку “OK“
 
-    check_text_in_result(wait, expected_result_confirm_text)
-    # Alert закрылся. В секции “Result“
-    # отображается текст “You clicked: Ok“
+    expected_result_confirm_text = "You clicked: Ok"
+    check_text_in_result(
+        actual_text=alert_page.text_in_result(),
+        expected_text=expected_result_confirm_text
+    )
+    # Alert закрылся. В секции “Result“ отображается текст “You clicked: Ok“
 
-    wait.until(EC.element_to_be_clickable(BUTTON_JS_PROMPT)).click()
+    alert_page.click_js_prompt()
     # Нажать кнопку “Click for JS Prompt“
 
-    alert_appearance(wait)
-    alert = wait.until(lambda d: d.switch_to.alert)
-    alert_text_verification(alert, expected_prompt_text)
-    # Отображается Alert с текстом “I am a
-    # JS Prompt“
+    expected_prompt_text = "I am a JS prompt"
+    # специально некорректный текст в ОР ТК: Prompt -> prompt ???))
+
+    alert_text_verification(
+        alert_text=alert_page.text_in_alert(),
+        expect_text=expected_prompt_text
+    )
+    # Отображается Alert с текстом “I am a JS Prompt“
 
     random_text = Faker().sentence(nb_words=2)
 
-    alert.send_keys(random_text)
-    alert.accept()
-    # Ввести в поле ввода случайно
-    # сгенерированный текст. Нажать кнопку
-    # “OK“
+    alert_page.send_text_to_alert(text=random_text)
+    alert_page.alert_accept()
+    # Ввести в поле ввода случайно сгенерированный текст. Нажать кнопку “OK“.
 
-    check_text_in_result(wait, f"You entered: {random_text}")
-    # Alert закрылся. В секции “Result“
-    # отображается текст “You entered:
-    # %введенный случайно
-    # сгенерированный текст%“
+    expected_result_prompt_text = "You entered: {}"
+
+    check_text_in_result(
+        actual_text=alert_page.text_in_result(),
+        expected_text=expected_result_prompt_text.format(random_text)
+    )
+    # Alert закрылся. В секции “Result“ отображается текст “You entered:
+    # %введенный случайно сгенерированный текст%“
